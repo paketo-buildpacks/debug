2a0044899fb25af081473ea9b9d4b8c39e171083
Ben Hale <bhale@pivotal.io>
2020-03-12 11:17:33 -0700

    Test Fix
    Signed-off-by: Ben Hale <bhale@pivotal.io>

--- debug/build_test.go
+++ debug/build_test.go
@@ -34,7 +34,7 @@ func testBuild(t *testing.T, context spec.G, it spec.S) {
 	)
 
 	it("does nothing without plan", func() {
-		Expect(build.Build(ctx)).To(Equal(BeZero()))
+		Expect(build.Build(ctx)).To(Equal(libcnb.BuildResult{}))
 	})
 
 	it("adds debug to the result", func() {
